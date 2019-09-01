dd2eb60c8b5a1bc5cc15d07a52b8f5349aee17cd
朱快 <zhukuai@git.wjr.com>
2019-09-01 22:24:24 +0800

    change crash dispose

--- ball/assets/Script/Ball.ts
+++ ball/assets/Script/Ball.ts
@@ -25,51 +25,118 @@ export class Ball extends cc.Component {
         }
         // return;
         this.lastOther = other;
+        const otherWorldPos: cc.Vec2 = other.node.convertToWorldSpaceAR(cc.v2(0, 0));
+        const meWorldPos: cc.Vec2 = me.node.convertToWorldSpaceAR(cc.v2(0, 0));
+        const vec: cc.Vec2 = cc.v2(otherWorldPos.x - meWorldPos.x, otherWorldPos.y - meWorldPos.y);
+        if (vec.x > 0) {
+            if (vec.x > Math.abs(vec.y)) {
+                // 右边碰到了
+                console.log("BBBB");
 
-        const meLeft: number = me.world.aabb.x;
-        const meRight: number = me.world.aabb.x + me.world.aabb.width;
-        const meTop: number = me.world.aabb.y + me.world.aabb.height;
-        const meBottom: number = me.world.aabb.y;
+                this.speedX = -10;
+                this.speed = cc.v2(this.speedX, this.speedY);
 
-        const otherLeft: number = other.world.aabb.x;
-        const otherRight: number = other.world.aabb.x + other.world.aabb.width;
-        const otherTop: number = other.world.aabb.y + other.world.aabb.height;
-        const otherBottom: number = other.world.aabb.y;
-        if (meLeft < otherRight && meRight > otherRight && meTop < otherBottom && meBottom > otherTop) {
-            // 左边碰到了
-            console.log("AAAA");
-            this.speedX = 2;
-            this.speed = cc.v2(this.speedX, this.speedY);
+                return;
+            } else {
+                if (vec.y > 0) {
+                    // 上边碰到了
+                    console.log("CCCC111");
 
-            return;
+                    this.speedY = -5;
+                    this.speed = cc.v2(this.speedX, this.speedY);
+
+                    return;
+
+                } else {
+                    // 下边碰到了
+                    console.log("DDDD222");
+
+                    this.speedY = 5;
+                    this.speed = cc.v2(this.speedX, this.speedY);
+
+                    return;
+
+                }
+            }
+        } else {
+            if (-vec.x > Math.abs(vec.y)) {
+                // 左边碰到了
+                console.log("AAAA");
+                this.speedX = 10;
+                this.speed = cc.v2(this.speedX, this.speedY);
+
+                return;
+            } else {
+                if (vec.y > 0) {
+                    // 上边碰到了
+                    console.log("CCCC222");
+
+                    this.speedY = -5;
+                    this.speed = cc.v2(this.speedX, this.speedY);
+
+                    return;
+
+                } else {
+                    // 下边碰到了
+                    console.log("DDDD222");
+
+                    this.speedY = 5;
+                    this.speed = cc.v2(this.speedX, this.speedY);
+
+                    return;
+
+                }
+            }
         }
-        if (meRight > otherLeft && meLeft < otherLeft && meTop < otherBottom && meBottom > otherTop) {
-            // 右边碰到了
-            console.log("BBBB");
 
-            this.speedX = -2;
-            this.speed = cc.v2(this.speedX, this.speedY);
+        // const meLeft: number = me.world.aabb.x;
+        // const meRight: number = me.world.aabb.x + me.world.aabb.width;
+        // const meTop: number = me.world.aabb.y + me.world.aabb.height;
+        // const meBottom: number = me.world.aabb.y;
 
-            return;
-        }
-        if (meTop > otherBottom && meBottom < otherBottom && meLeft > otherRight && meRight < otherLeft) {
-            // 顶上碰到了
-            console.log("CCCC");
+        // const otherLeft: number = other.world.aabb.x;
+        // const otherRight: number = other.world.aabb.x + other.world.aabb.width;
+        // const otherTop: number = other.world.aabb.y + other.world.aabb.height;
+        // const otherBottom: number = other.world.aabb.y;
 
-            this.speedY = -1;
-            this.speed = cc.v2(this.speedX, this.speedY);
+        // if (meLeft < otherRight && meRight > otherRight && meTop < otherBottom && meBottom > otherTop) {
+        //     // 左边碰到了
+        //     console.log("AAAA");
+        //     this.speedX = 2;
+        //     this.speed = cc.v2(this.speedX, this.speedY);
 
-            return;
-        }
-        if (meBottom < otherTop && meTop > otherTop && meLeft > otherRight && meRight < otherLeft) {
-            // 底下碰到了
-            console.log("DDDD");
+        //     return;
+        // }
+        // if (meRight > otherLeft && meLeft < otherLeft && meTop < otherBottom && meBottom > otherTop) {
+        //     // 右边碰到了
+        //     console.log("BBBB");
 
-            this.speedY = 1;
-            this.speed = cc.v2(this.speedX, this.speedY);
+        //     this.speedX = -2;
+        //     this.speed = cc.v2(this.speedX, this.speedY);
 
-            return;
-        }
+        //     return;
+        // }
+        // if (meTop > otherBottom && meBottom < otherBottom && meLeft > otherRight && meRight < otherLeft) {
+        //     // 顶上碰到了
+        //     console.log("CCCC");
+
+        //     this.speedY = -1;
+        //     this.speed = cc.v2(this.speedX, this.speedY);
+
+        //     return;
+        // }
+        // if (meBottom < otherTop && meTop > otherTop && meLeft > otherRight && meRight < otherLeft) {
+        //     // 底下碰到了
+        //     console.log("DDDD");
+
+        //     this.speedY = 1;
+        //     this.speed = cc.v2(this.speedX, this.speedY);
+
+        //     return;
+        // }
+
+
+
         // const otherWorldPos: cc.Vec2 = other.node.convertToWorldSpaceAR(cc.v2(0, 0));
         // const meWorldPos: cc.Vec2 = me.node.convertToWorldSpaceAR(cc.v2(0, 0));
         // const deltaX: number = otherWorldPos.x - meWorldPos.x;
